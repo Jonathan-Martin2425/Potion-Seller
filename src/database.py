0366cf7170996b0f0e4cdbe5cfdecf8434900f95
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-09-30 15:11:55 -0700

    commit #3

--- src/database.py
+++ src/database.py
@@ -6,4 +6,4 @@ def database_connection_url():
     dotenv.load_dotenv()
     return os.environ.get("POSTGRES_URI")
 
-engine = create_engine("postgresql://postgres.lszbbhqpvvcfxicvybyw:zF_9uGSj64zgYKB@aws-0-us-west-1.pooler.supabase.com:6543/postgres", pool_pre_ping=True)
+engine = create_engine(database_connection_url(), pool_pre_ping=True)

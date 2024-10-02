75b1f90d07edc650135c028fa1295143c7fd87dd
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-10-02 09:38:01 -0700

    commit #8

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -53,7 +53,7 @@ def get_bottle_plan():
         return [
                 {
                     "potion_type": [0, 100, 0, 0],
-                    "quantity": 5,
+                    "quantity": 1,
                 }
             ]
     else:

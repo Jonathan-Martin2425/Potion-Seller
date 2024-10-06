ab804748173f894ec0d94160706b2330fccb851c
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-10-06 01:25:12 -0700

    commit #17

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,32 +10,34 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
+    res = []
     with db.engine.begin() as connection:
-        potions = connection.execute(sqlalchemy.text("SELECT potions FROM global_inventory")).scalar()
 
-    if potions > 0:
-        return [
-                {
+        # checks quantity of all potion types and adds
+        # potions type to catalog if there >0 in the inventory
+        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= red")).scalar() > 0:
+            res.append({
                     "sku": "RED_POTION",
                     "name": "red potion",
                     "quantity": 1,
                     "price": 40,
                     "potion_type": [100, 0, 0, 0],
-                },
-                {
+                })
+        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= green")).scalar() > 0:
+            res.append({
                     "sku": "GREEN_POTION",
                     "name": "green potion",
                     "quantity": 1,
                     "price": 40,
                     "potion_type": [0, 100, 0, 0],
-                },
-                {
+                })
+        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= blue")).scalar() > 0:
+            res.append({
                     "sku": "BLUE_POTION",
                     "name": "blue potion",
                     "quantity": 1,
                     "price": 40,
                     "potion_type": [0, 0, 100, 0],
-                }
-            ]
-    else:
-        return []
+                })
+
+    return res

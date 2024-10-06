8ca145f301081975b9ac31a7be11013c29c17d5f
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-10-06 12:01:50 -0700

    commit #17

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,7 +15,7 @@ def get_catalog():
 
         # checks quantity of all potion types and adds
         # potions type to catalog if there >0 in the inventory
-        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= red")).scalar() > 0:
+        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= 'red'")).scalar() > 0:
             res.append({
                     "sku": "RED_POTION",
                     "name": "red potion",
@@ -23,7 +23,7 @@ def get_catalog():
                     "price": 40,
                     "potion_type": [100, 0, 0, 0],
                 })
-        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= green")).scalar() > 0:
+        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= 'green'")).scalar() > 0:
             res.append({
                     "sku": "GREEN_POTION",
                     "name": "green potion",
@@ -31,7 +31,7 @@ def get_catalog():
                     "price": 40,
                     "potion_type": [0, 100, 0, 0],
                 })
-        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= blue")).scalar() > 0:
+        if connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE potion_type= 'blue'")).scalar() > 0:
             res.append({
                     "sku": "BLUE_POTION",
                     "name": "blue potion",

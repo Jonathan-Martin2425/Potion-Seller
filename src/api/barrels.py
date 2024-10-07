58cf76ae8be818861a82976cf1db50a8576a1994
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-10-07 10:39:48 -0700

    Version #2 #commit 4

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -20,6 +20,7 @@ class Barrel(BaseModel):
 
     quantity: int
 
+
 # given the parameters for the correct API response
 # returns the correct json format
 def potion_json(item_sku: str, quantity: int):
@@ -27,49 +28,51 @@ def potion_json(item_sku: str, quantity: int):
             "quantity": quantity}
 
 
+#takes in a list and returns the min index
+#used for finding min ml of all barrel types
+def min_barrels(barrel_types: list) -> int:
+    m = 0
+    cur = 0
+    for t in barrel_types:
+        if barrel_types[m] > t:
+            m = cur
+        cur += 1
+    return m
+
+
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
-    barrels_types = []
+    barrel_types = []
     with db.engine.begin() as connection:
         ml = connection.execute(sqlalchemy.text("SELECT ml FROM global_inventory")).scalar()
         gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
 
         # gets quantity of ml for each type
-        barrels_types.append(
+        barrel_types.append(
             connection.execute(sqlalchemy.text("SELECT ml FROM barrels WHERE barrel_type= 'red'")).scalar())
-        barrels_types.append(
+        barrel_types.append(
             connection.execute(sqlalchemy.text("SELECT ml FROM barrels WHERE barrel_type= 'green'")).scalar())
-        barrels_types.append(
+        barrel_types.append(
             connection.execute(sqlalchemy.text("SELECT ml FROM barrels WHERE barrel_type= 'blue'")).scalar())
 
     for b in barrels_delivered:
-        if b.potion_type[0] == 1:
-            barrels_types[0] += b.ml_per_barrel
-            ml += b.ml_per_barrel
-            gold -= b.price
-        if b.potion_type[1] == 1:
-            barrels_types[1] += b.ml_per_barrel
-            ml += b.ml_per_barrel
-            gold -= b.price
-        if b.potion_type[2] == 1:
-            barrels_types[2] += b.ml_per_barrel
-            ml += b.ml_per_barrel
-            gold -= b.price
+        for i in range(3):
+            if b.potion_type[i] == 1:
+                barrel_types[i] += b.ml_per_barrel
+                ml += b.ml_per_barrel
+                gold -= b.price
 
     # what does "OK" do in a Json package/SQL excecution
     # "OK" tells the reciever that no error occurred
 
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET ml = {ml} WHERE id= 1"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = {gold} WHERE id= 1"))
-        connection.execute(sqlalchemy.text(f"UPDATE barrels SET ml = {barrels_types[0]} WHERE barrel_type= 'red'"))
-        connection.execute(sqlalchemy.text(f"UPDATE barrels SET ml = {barrels_types[1]} WHERE barrel_type= 'green'"))
-        connection.execute(sqlalchemy.text(f"UPDATE barrels SET ml = {barrels_types[2]} WHERE barrel_type= 'blue'"))
-
-
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET ml = {ml}, gold = {gold} WHERE id= 1"))
+        connection.execute(sqlalchemy.text(f"UPDATE barrels SET ml = {barrel_types[0]} WHERE barrel_type= 'red'"))
+        connection.execute(sqlalchemy.text(f"UPDATE barrels SET ml = {barrel_types[1]} WHERE barrel_type= 'green'"))
+        connection.execute(sqlalchemy.text(f"UPDATE barrels SET ml = {barrel_types[2]} WHERE barrel_type= 'blue'"))
 
     return []
 
@@ -79,20 +82,25 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
+    barrel_types = []
     with db.engine.begin() as connection:
         potions = connection.execute(sqlalchemy.text("SELECT potions FROM global_inventory")).scalar()
         gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
+        # gets quantity of ml for each type
+        barrel_types.append(
+            connection.execute(sqlalchemy.text("SELECT ml FROM barrels WHERE barrel_type= 'red'")).scalar())
+        barrel_types.append(
+            connection.execute(sqlalchemy.text("SELECT ml FROM barrels WHERE barrel_type= 'green'")).scalar())
+        barrel_types.append(
+            connection.execute(sqlalchemy.text("SELECT ml FROM barrels WHERE barrel_type= 'blue'")).scalar())
 
     potion_skus = [""] * 3
     for barrel in wholesale_catalog:
-            if barrel.potion_type[0] == 1 and barrel.price <= gold:
-                potion_skus[0] = barrel.sku
-                gold -= barrel.price
-            if barrel.potion_type[1] == 1 and barrel.price <= gold:
-                potion_skus = barrel.sku
-                gold -= barrel.price
-            if barrel.potion_type[2] == 1 and barrel.price <= gold:
-                potion_skus = barrel.sku
+        min_ml = min_barrels(barrel_types)
+        for i in range(3):
+            if barrel.potion_type[i] == 1 and gold >= barrel.price and\
+                    (min_ml == i or gold >= barrel.price * 2):
+                potion_skus[i] = barrel.sku
                 gold -= barrel.price
     res = []
 

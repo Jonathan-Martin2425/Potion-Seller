fca51ec77ecd2894240ee6bbbc238ff0ccc6b39b
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-11-07 08:46:49 -0800

    Version #3 commit #7

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -11,6 +11,7 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
+
 @router.get("/audit")
 def get_inventory():
     """ """
@@ -20,6 +21,7 @@ def get_inventory():
         gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
         return {"number_of_potions": potions, "ml_in_barrels": ml, "gold": gold}
 
+
 # Gets called once a day
 @router.post("/plan")
 def get_capacity_plan():
@@ -27,19 +29,34 @@ def get_capacity_plan():
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
+    with db.engine.begin() as connection:
+        gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
 
-    return {
-        "potion_capacity": 0,
-        "ml_capacity": 0
-        }
+        if gold > 5000:
+            return {
+                "potion_capacity": 2,
+                "ml_capacity": 2
+            }
+        elif gold > 2500:
+            return {
+                "potion_capacity": 1,
+                "ml_capacity": 1
+            }
+        else:
+            return {
+                "potion_capacity": 0,
+                "ml_capacity": 0
+            }
+
 
 class CapacityPurchase(BaseModel):
     potion_capacity: int
     ml_capacity: int
 
+
 # Gets called once a day
 @router.post("/deliver/{order_id}")
-def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
+def deliver_capacity_plan(capacity_purchase: CapacityPurchase, order_id: int):
     """ 
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
